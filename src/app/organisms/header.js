36e8eaa908560b30814d0a53a0ef4d1bade250e4
Lee Crosby <leemcrosby@googlemail.com>
2017-02-13 10:25:26 +0000

    main layout

--- src/app/organisms/header.js
+++ src/app/organisms/header.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
-import Styles from './header.css';
+import styles from './header.less';
 import {Mediaobject} from '../molecules/mediaobject';
+import {Glyphicon, InputGroup, FormGroup, FormControl} from 'react-bootstrap';
 
 export class Header extends Component {
   render() {
     return (
-      <header className={Styles.header}>
-        <Mediaobject image={{src: '../content/images/rezi-logo.svg', width: '24'}} text="dezrez services ltd."/>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Mediaobject image={{src: '../content/images/rezi-logo.svg', width: '24'}} text="dezrez services ltd."/>
+        </div>
+        <div className={styles.searchContainer}>
+          <form>
+            <FormGroup>
+              <InputGroup>
+                <FormControl type="search" placeholder="Search for..."/>
+                <InputGroup.Addon><Glyphicon glyph="search"/></InputGroup.Addon>
+              </InputGroup>
+            </FormGroup>
+          </form>
+        </div>
+        <div className={styles.globalToolsContainer}></div>
       </header>
     );
   }
--- src/app/organisms/sidebar.js
+++ src/app/organisms/sidebar.js
@@ -7,6 +7,8 @@ export class Sidebar extends Component {
   render() {
     return (
       <nav className={styles.container}>
+        <div className={styles.logo}>
+        </div>
         <ul role="nav" className={styles.list}>
           <li className={styles.listItem}><IndexLink to="/" activeClassName={styles.active}>Home</IndexLink></li>
           <li className={styles.listItem}><NavLink to="/components" activeClassName={styles.active}>Components</NavLink></li>
